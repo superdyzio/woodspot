5dc3dc3d4cc2a030f06d3de9cd6a4eef0b4193bd
Dawid Perdek <perdekdawid@gmail.com>
2023-06-03 20:31:30 +0200

    header done

--- pages/index.js
+++ pages/index.js
@@ -1,116 +1,125 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { Dropdown } from '@nextui-org/react';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Woodspot</title>
+                <link rel="icon" href="/logo.svg"/>
+            </Head>
 
-      <main>
-        <div>TEST TEST TEST</div>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main>
+                <header className={styles.header}>
+                    <img src={'/logo.svg'} alt={'woodspot logo'}/>
+                    <nav>
+                        <a href="#paintball">PAINTBALL</a>
+                        <a href="">ŁOWISKA</a>
+                        <a href="">BUSH&FUN</a>
+                        <a href="">O NAS</a>
+                        <a href="">KONTAKT</a>
+                    </nav>
+                    <div className="hamburger">
+                        <Dropdown placement='bottom-right'>
+                            <Dropdown.Button light iconRight={<span></span>}>
+                                <div className='hamburger__icon'>
+                                    <svg className="w-8 h-8 text-gray-500" fill="none" strokeLinecap="round"
+                                         strokeLinejoin="round" strokeWidth="2"
+                                         viewBox="0 0 24 24" stroke="white">
+                                        <path d="M4 6h16M4 12h16M4 18h16"></path>
+                                    </svg>
+                                </div>
+                            </Dropdown.Button>
+                            <Dropdown.Menu aria-label="navigation actions">
+                                <Dropdown.Item key="new">
+                                    <a href="#paintball">PAINTBALL</a>
+                                </Dropdown.Item>
+                                <Dropdown.Item key="copy">
+                                    <a href="">ŁOWISKA</a>
+                                </Dropdown.Item>
+                                <Dropdown.Item key="edit">
+                                    <a href="">BUSH&FUN</a>
+                                </Dropdown.Item>
+                                <Dropdown.Item key="delete">
+                                    <a href="">O NAS</a>
+                                </Dropdown.Item>
+                                <Dropdown.Item key="contact">
+                                    <a href="">KONTAKT</a>
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                </header>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+                <section id="paintball">Paintball</section>
+                <section>Łowienie</section>
+            </main>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <style jsx>{`
+              main {
+                width: 100%;
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+                justify-content: start;
+                align-items: center;
+              }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+              main img {
+                height: 50px;
+                z-index: 9;
+              }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+              .hamburger {
+                display: none;
+                height: 50px;
+                justify-content: end;
+                width: 100%;
+                z-index: 9;
+              }
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              .hamburger__icon {
+                height: 40px;
+                width: 40px;
+                z-index: 9;
+              }
+              
+              @media (max-width: 1110px) {
+                .hamburger {
+                  display: flex;
+                }
+              }
+
+              @media (max-width: 500px) {
+                main img {
+                  height: 30px;
+                  margin-top: 10px;
+                }
+              }
+            `}</style>
+
+            <style jsx global>{`
+              html,
+              body {
+                padding: 0;
+                margin: 0;
+                font-family: "Trebuchet MS", -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+                background-color: #002902;
+              }
+
+              * {
+                box-sizing: border-box;
+              }
+
+              li span a {
+                color: #002902;
+                text-decoration: none;
+                cursor: pointer;
+              }
+            `}</style>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    );
 }
